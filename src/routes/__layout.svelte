7b711c0927d55bfd62a77c6a64b1828a496c3d30
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-03-13 22:38:47 +0200

    More appropriate use of layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,5 @@
-<script lang="ts">
-	import Header from '../ui/Header.svelte';
-	import Board from '../ui/Board.svelte';
-</script>
-
 <svelte:head>
 	<title>Sliding tiles</title>
 </svelte:head>
 
-<div>
-	<Header />
-	<Board />
-</div>
-
-<style>
-	div {
-		height: 100vh;
-		display: grid;
-		grid-template-rows: 5vh 90vh;
-	}
-</style>
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from '../ui/Header.svelte';
+	import Board from '../ui/Board.svelte';
+</script>
+
+<div>
+	<Header />
+	<Board />
+</div>
+
+<style>
+	div {
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 5vh 90vh;
+	}
+</style>
