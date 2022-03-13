9e90034ebbd12dfcc02447f91715dc5f6216982d
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-03-13 14:57:15 +0200

    streamline store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,14 @@
-import { get, writable, derived } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 import { create, shuffle, movable, swap } from '../lib';
 
 const INITIAL_SIZE = 4;
 
-export const size = writable(0);
+export const size = writable(INITIAL_SIZE);
 export const tiles = writable<number[]>([]);
-export const legal = derived(tiles, ($tiles) => movable($tiles, get(size)));
+export const legal = derived([tiles, size], ([$tiles, $size]) => movable($tiles, $size));
 
-export const init = () => tiles.set(shuffle(create(get(size)), get(size)));
+export const init = (size: number) => tiles.set(shuffle(create(size), size));
 
 export const move = (tile: number) => tiles.update((tiles) => swap([...tiles], tile));
 
 size.subscribe(init);
-
-size.set(INITIAL_SIZE);
